b5d89ce9fd8906b02df0e71cee358664f272ff6e
harshith reddy chiluka <harshithkalki@gmail.com>
2025-07-03 00:24:40 -0400

    changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,30 +8,35 @@ import CredentialsProvider from "next-auth/providers/credentials";
 const authcopnfig: NextAuthConfig = {
   providers: [
     CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email", placeholder: "you@example.com" },
-        password: { label: "Password", type: "string" },
-      },
-      async authorize(credentials: { email?: string; password?: string }){
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Email and password are required");
-        }   
-         const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        });
-        if (!user) {
-          throw new Error("No user found with the given email");
-        }
-        const isValidPassword = await compare(credentials.password, user.password);
-        if (!isValidPassword) {
-          throw new Error("Invalid password");
-        }
-        return { id: user.id, email: user.email };
-      },
-    }),
+        name: "Credentials",
+        credentials: {
+          email: { label: "Email", type: "text" },
+          password: { label: "Password", type: "password" },
+        },
+        async authorize(credentials: { email?: string; password?: string }) {
+          if (!credentials?.email || !credentials?.password) {
+            throw new Error("Missing email or password");
+          }
+      
+          const user = await prisma.user.findUnique({
+            where: { email: credentials.email }, 
+          });
+      
+          if (!user) throw new Error("No user found");
+          const isValid = await compare(credentials.password, user.password);
+          if (!isValid) throw new Error("Invalid password");
+      
+          return {
+            id: user.id.toString(),
+            name: user.name,
+            email: user.email,
+          };
+        },
+      })
+      
   ],
 
-
+}
 export const { GET, POST } = handlers;
+
     
